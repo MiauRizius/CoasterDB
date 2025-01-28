6c52a489c1c639be11b34d817979fa7fa84eceab
Maurice L. <maurice.lariviere@icloud.com>
2025-01-28 16:12:48 +0100

    Added some .github-Files

---
+++ .github/CONTRIBUTING.md
@@ -0,0 +1,10 @@
+## Making Changes
+
+Depending on your changes there are certain rules you have to follow if you expect your Pull Request (PR) to be merged.
+
+1. Check for existing pull requests that might already implement your changes.  
+    You can review those pull requests with your suggestions for improvements.
+1. Reference documentation or issues that are relevant to your changes.  
+1. If you implement a new feature, include some examples on how you intend it to be used in your PR description.
+1. Write documentation for all public API methods and types.
+1. Follow the coding style, even if unconventional, we want to keep it consistent to maintain readability.
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,15 +36,15 @@ console.log('%c Wenn du nicht 100% weißt, was du tust, schließe dieses Fenster
                 <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
                     <dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
                         <div class="flex flex-col items-center justify-center">
-                            <dt class="mb-2 text-3xl md:text-4xl font-extrabold">3.543</dt>
+                            <dt class="mb-2 text-3xl md:text-4xl font-extrabold">27</dt>
                             <dd class="font-light text-gray-500 dark:text-gray-400">Parks</dd>
                         </div>
                         <div class="flex flex-col items-center justify-center">
-                            <dt class="mb-2 text-3xl md:text-4xl font-extrabold">453.567</dt>
+                            <dt class="mb-2 text-3xl md:text-4xl font-extrabold">400</dt>
                             <dd class="font-light text-gray-500 dark:text-gray-400">Achterbahnen</dd>
                         </div>
                         <div class="flex flex-col items-center justify-center">
-                            <dt class="mb-2 text-3xl md:text-4xl font-extrabold">5</dt>
+                            <dt class="mb-2 text-3xl md:text-4xl font-extrabold">785</dt>
                             <dd class="font-light text-gray-500 dark:text-gray-400">Datenbankeinträge</dd>
                         </div>
                     </dl>
